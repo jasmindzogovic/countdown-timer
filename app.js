2bb9c9d14d79deb09c2abe8d89b41071fc42e9dd
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-10-09 23:41:32 -0400

    added text content dynamically to DOM

--- app.js
+++ app.js
@@ -21,3 +21,20 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2023, 9, 24, 8, 0, 0);
+
+const year = futureDate.getFullYear();
+const month = futureDate.getMonth();
+const date = futureDate.getDate();
+const day = futureDate.getDay();
+const hour = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+giveaway.textContent = `Giveaway ends on ${weekdays[day]}, ${
+  months[month]
+} ${date} ${year}, ${hour}:${String(minutes).padStart(2, 0)} AM`;
